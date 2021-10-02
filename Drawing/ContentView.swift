9a7369410bc4204b0dab543e53c0784f0769e854
1Soyebo <soyeboibukun@gmail.com>
2021-10-02 20:14:53 +0100

    added border picgures

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -49,18 +49,10 @@ struct ContentView: View {
     @State private var petalWidth = 100.0
 
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("example"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
 
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
     }
 }
 
