49ac8f4fe4f9b22c1bee76c445c6ec87d48aaec5
1Soyebo <soyeboibukun@gmail.com>
2021-10-02 21:45:17 +0100

    animatable data with checkboard

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -20,6 +20,67 @@ struct Triangle: Shape {
     }
 }
 
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
+
 struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
@@ -45,16 +106,18 @@ struct Arc: InsettableShape {
 }
 
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.9
+    @State private var rows = 4
+    @State private var columns = 4
 
-    var body: some View {
-        Image("example")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 200, height: 200)
-            .saturation(Double(amount))
-            .blur(radius: (1 - amount) * 20)
-    }
+       var body: some View {
+           Checkerboard(rows: rows, columns: columns)
+               .onTapGesture {
+                   withAnimation(.linear(duration: 3)) {
+                       self.rows = 8
+                       self.columns = 16
+                   }
+               }
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
